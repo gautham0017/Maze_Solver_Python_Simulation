662f699a5527cee5eba2f26fb04f134ef3c75de0
gautham0017 <gauthamgk2232006@gmail.com>
2026-08-02 00:03:14 +0530

    Update MazeLoader.py

--- MazeLoader.py
+++ MazeLoader.py
@@ -1,28 +1,13 @@
 import json
-
 from Config import Direction
 from Maze import loadMaze
 
-
 def loadMazeFromFile(filename="maze_config.json"):
-
     with open(filename, "r") as file:
         data = json.load(file)
-
     walls = []
-
     for wall in data["walls"]:
-
         direction = Direction[wall["direction"]]
-
-        walls.append(
-            (
-                wall["row"],
-                wall["col"],
-                direction
-            )
-        )
-
+        walls.append((wall["row"],wall["col"],direction))
     loadMaze(walls)
-
     return data
